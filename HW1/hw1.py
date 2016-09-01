ab661a99b236e7342e1dc93b6fb1515af70e6bb9
zhouyichen <zhouyichen123@gmail.com>
2016-09-01 15:56:29 +0800

    [HW1] complete part a, b, c in python file

---
+++ HW1/hw1.py
@@ -0,0 +1,90 @@
+import numpy as np
+import numpy.random as nr
+import matplotlib.pyplot as pl
+# %matplotlib inline
+# Plotting with style! 
+import seaborn as sb 
+
+# Size the plot appropriately for online display
+pl.rcParams['figure.figsize'] = (12.0, 10.0)
+
+nr.seed(3244)
+
+#    Write your solution to the programming assignment here.  We've suggested some cells that you can add 
+#    to your notebook as single line comments below.
+#    Please place all of your cells to be run in a linear, unintervened order, such that we can automate
+#    the running and grading of the assignment.
+
+# load datasets code
+from io import StringIO
+
+def read_data_file(file_name):
+    f = open(file_name, "r")
+    mat = np.loadtxt(StringIO(f.read()))
+    X = mat[:, :-1]
+    y = np.matrix(mat[:, -1]).T
+    return (X, y)
+
+X_train, Y_train = read_data_file("hw1-train.dat")
+N, M = X_train.shape
+
+X_test, Y_test = read_data_file("hw1-test.dat")
+
+# LR code
+
+def lr(xn, yn, w, eta, index=-1):
+    '''
+        Input: 
+            xn : Data points
+            yn : Classification of the previous data points, an Nx1 vector. 
+            w  : initial weight
+            eta : step size
+            index: index of the datapoint chosen, -1 means it will be random
+        Output: 
+            w : updated weight
+    '''
+    N = xn.shape[0]
+    if index == -1: # stochastic
+        index = nr.randint(N)
+    x = xn[index, :]
+    y = yn[index]
+    g = y * x / (1 + np.exp(y * (x.dot(w))))
+    w = w.T + eta * g
+    return w.T
+    
+# Evaluation code
+def calculate_error(xn, yn, w):
+    N = xn.shape[0]
+    e = np.sum(np.log(1 + np.exp(np.multiply(-yn, (np.dot(xn, w)))))) / N
+    print(e)
+    return e
+
+def evaluate_stochastic(eta, iteration):
+	w = np.zeros([M, 1])
+	for i in range(iteration):
+	    calculate_error(X_train, Y_train, w)
+	    w = lr(X_train, Y_train, w, eta, -1)
+	E_out = calculate_error(X_test, Y_test, w)
+	return (w, E_out)
+
+def evaluate_deterministic(eta, iteration):
+	w = np.zeros([M, 1])
+	for i in range(iteration):
+	    calculate_error(X_train, Y_train, w)
+	    w = lr(X_train, Y_train, w, eta, i % N)
+	E_out = calculate_error(X_test, Y_test, w)
+	return (w, E_out)
+
+w, E_out = evaluate_stochastic(0.05, 2333)
+print(w, E_out)
+
+w, E_out = evaluate_stochastic(0.005, 2333)
+print(w, E_out)
+
+w, E_out = evaluate_deterministic(0.05, 2333)
+print(w, E_out)
+
+w, E_out = evaluate_deterministic(0.005, 2333)
+print(w, E_out)
+
+# Plotting (if any) code

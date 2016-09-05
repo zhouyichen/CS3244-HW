5f07f853cebc308d2feeb6c40d42e061a57f9d8c
zhouyichen <zhouyichen123@gmail.com>
2016-09-05 13:18:28 +0800

    [HW1] add better print statements

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -19,11 +19,11 @@ nr.seed(3244)
 from io import StringIO
 
 def read_data_file(file_name):
-    f = open(file_name, "r")
-    mat = np.loadtxt(StringIO(f.read()))
-    X = mat[:, :-1]
-    y = np.matrix(mat[:, -1]).T
-    return (X, y)
+	f = open(file_name, "r")
+	mat = np.loadtxt(StringIO(f.read()))
+	X = mat[:, :-1]
+	y = np.matrix(mat[:, -1]).T
+	return (X, y)
 
 X_train, Y_train = read_data_file("hw1-train.dat")
 N, M = X_train.shape
@@ -33,31 +33,33 @@ X_test, Y_test = read_data_file("hw1-test.dat")
 # LR code
 
 def lr(xn, yn, w, eta, index=-1):
-    '''
-        Input: 
-            xn : Data points
-            yn : Classification of the previous data points, an Nx1 vector. 
-            w  : initial weight
-            eta : step size
-            index: index of the datapoint chosen, -1 means it will be random
-        Output: 
-            w : updated weight
-    '''
-    N = xn.shape[0]
-    if index == -1: # stochastic
-        index = nr.randint(N)
-    x = xn[index, :]
-    y = yn[index]
-    g = y * x / (1 + np.exp(y * (x.dot(w))))
-    w = w.T + eta * g
-    return w.T
+	'''
+	    Input: 
+	        xn : Data points
+	        yn : Classification of the previous data points, an Nx1 vector. 
+	        w  : initial weight
+	        eta : step size
+	        index: index of the datapoint chosen, -1 means it will be random
+	    Output: 
+	        w : updated weight
+	'''
+	N = xn.shape[0]
+	if index == -1: # stochastic
+	    index = nr.randint(N)
+	x = xn[index, :]
+	y = yn[index]
+	g = y * x / (1 + np.exp(y * (x.dot(w))))
+	w = w + eta * g.T
+	return w
     
 # Evaluation code
+
+# calculate the in sample error given w
 def calculate_error(xn, yn, w):
-    N = xn.shape[0]
-    e = np.sum(np.log(1 + np.exp(np.multiply(-yn, (np.dot(xn, w)))))) / N
-    # print(e)
-    return e
+	N = xn.shape[0]
+	e = np.sum(np.log(1 + np.exp(np.multiply(-yn, (np.dot(xn, w)))))) / N
+	# print(e)
+	return e
 
 # sigmoid function which can be applied to a vector
 def sigmoid(s):
@@ -66,11 +68,11 @@ def sigmoid(s):
 
 # calculates the E_out given data, classification, and weights
 def out_error(xn, yn, w):
-    N = xn.shape[0]
-    prediction = (sigmoid(np.dot(xn, w)) > 0.5)
-    real_class = (yn > 0)
-    e = sum(prediction != real_class) / N
-    return e
+	N = xn.shape[0]
+	prediction = (sigmoid(np.dot(xn, w)) > 0.5)
+	real_class = (yn > 0)
+	e = np.sum(prediction != real_class) / N
+	return e
 
 def evaluate_stochastic(eta, iteration):
 	'''
@@ -101,21 +103,34 @@ def evaluate_deterministic(eta, iteration):
 	'''
 	w = np.zeros([M, 1])
 	for i in range(iteration):
-	    # calculate_error(X_train, Y_train, w)
-	    w = lr(X_train, Y_train, w, eta, i % N)
+		# calculate_error(X_train, Y_train, w)
+		w = lr(X_train, Y_train, w, eta, i % N)
 	E_out = out_error(X_test, Y_test, w)
 	return (w, E_out)
 
+
 w, E_out = evaluate_stochastic(0.05, 2333)
-print(w, E_out)
+print("a: eta = 0.05, T = 2333")
+print("Eventual weight vector:")
+print(w)
+print("E_out:", E_out, "\n")
 
 w, E_out = evaluate_stochastic(0.005, 2333)
-print(w, E_out)
+print("b: eta = 0.005, T = 2333")
+print("Eventual weight vector:")
+print(w)
+print("E_out:", E_out, "\n")
 
 w, E_out = evaluate_deterministic(0.05, 2333)
-print(w, E_out)
+print("c1: eta = 0.05, T = 2333, deterministic")
+print("Eventual weight vector:")
+print(w)
+print("E_out:", E_out, "\n")
 
 w, E_out = evaluate_deterministic(0.005, 2333)
-print(w, E_out)
+print("c2: eta = 0.05, T = 2333, deterministic")
+print("Eventual weight vector:")
+print(w)
+print("E_out:", E_out, "\n")
 
 # Plotting (if any) code

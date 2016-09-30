6a0288465789ee6603b9607d1c453468ff7016d6
zhouyichen <zhouyichen123@gmail.com>
2016-09-30 17:57:53 +0800

    [HW2] read data for q1

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,3 +1,14 @@
+import numpy as np
+from io import StringIO
+
+def read_q1_file(file_name):
+	f = open(file_name, "r")
+	mat = np.loadtxt(StringIO(f.read()))
+	X = mat[:, 1:]
+	# X = np.insert(X, 0, 1, axis = 1)
+	y = np.matrix(mat[:, 0]).T
+	return (X, y)
+
 def run_svm_and_write_output(q_number, dataTrain, labelTrain, dataTest, labelTest, cost, kernel, gamma, degree):
 	### The output of your program should be written in a file as follows.
 	#   for question 'i', write the output in 'problem-i.txt' file (e.g., 'problem-1a.txt')
@@ -20,3 +31,6 @@ def run_svm_and_write_output(q_number, dataTrain, labelTrain, dataTest, labelTes
 	fo.write("Number of Support Vectors: "+ str(totalSV)+"\n")
 	fo.write("Train Accuracy: "+ str(trainAccuracy)+"\n")
 	fo.write("Test Accuracy: " + str(testAccuracy)+"\n")
+
+q1_train_data, q1_train_lable = read_q1_file('hw2-1-train.txt')
+q1_test_data, q1_test_lable = read_q1_file('hw2-1-test.txt')
